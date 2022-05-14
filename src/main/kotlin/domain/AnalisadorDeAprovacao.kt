b7300144ae1fb1951086f02aaae8aef435046d23
ArthurPilone <art.pilone@gmail.com>
2022-05-13 21:14:21 -0300

    Implement AnalisadorDeAprovacao class
      It is also worth noting that the gradle-wrapper.properties
    file was also changed in order to indentify the gradle version
    used locally :)
    
    Signed-off-by: ArthurPilone <art.pilone@gmail.com>

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,15 +1,25 @@
 package domain
 
+import domain.criterios.CriterioDeAprovacao
+import domain.Boletim
+import domain.BoletimFechado
+
 class AnalisadorDeAprovacao {
 
-    // ---------------------------------
-    //
-    //      Seu código deve vir aqui
-    //
-    //      Defina atributos e métodos conforme especificado
-    //      no arquivo de teste encontrado em
-    //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
-    //
-    // ---------------------------------
+    private lateinit var meuCriterio: CriterioDeAprovacao
+
+    fun defineCriterio(criterio: CriterioDeAprovacao) {
+        meuCriterio = criterio
+    }
+
+    fun fechaBoletim(boletim: Boletim): BoletimFechado {
+        if(::meuCriterio.isInitialized){
+            return BoletimFechado(boletim.mediaEPs, boletim.mediaMiniEPs,
+                                  meuCriterio.mediaFinal(boletim),
+                                  meuCriterio.estaAprovado(boletim))
+        }else{
+            throw Exception("Tentativa de fechar boletim antes de definir critério")
+        }
+    }
 
 }
